ae1c2fa73667a6f427c0c3a9cf75364cd4cb6faf
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-09-17 19:26:54 -0300

    add link api em production

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api-vortice-tell-me-the-model.onrender.com/",
   withCredentials: true,
 })
