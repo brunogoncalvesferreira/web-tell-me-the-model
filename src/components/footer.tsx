2448f21cee82ce130f235246255beded35d40aed
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-10-23 18:41:59 -0300

    adicionando novas páginas

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,30 @@
+import { Link } from "react-router-dom";
+
 export function Footer() {
   return (
-    <footer className="flex  flex-col md:flex-row gap-4 items-center justify-between py-4 mt-10">
+    <footer className="flex  flex-col gap-4 items-center justify-between py-4 mt-10">
       <div>
-        <span className="text-zinc-300 text-sm font-light">Vortice Consulting & Tech -</span>
-        <span className="text-zinc-300 text-sm font-light"> CNPJ: 38.414.530/0001-59</span>
+        <span className="text-zinc-300 text-sm font-light">
+          Vortice Consulting & Tech -
+        </span>
+        <span className="text-zinc-300 text-sm font-light">
+          {" "}
+          CNPJ: 38.414.530/0001-59
+        </span>
       </div>
-      
+
+      <div className="space-x-4">
+        <Link className="text-sm text-zinc-300" to="/termos-e-condicoes">
+          Termos e Condições
+        </Link>
+        <Link className="text-sm text-zinc-300" to="/politica-de-privacidade">
+          Política de Privacidade
+        </Link>
+      </div>
+
       <p className="text-zinc-300 text-sm font-light">
         &copy; {new Date().getFullYear()} - Todos os Direitos Reservados
       </p>
-
     </footer>
-  )
+  );
 }
--- src/router/app-routes.tsx
+++ src/router/app-routes.tsx
@@ -4,6 +4,10 @@ import { CreateCompatibleModel } from "../pages/create-compatible-model";
 import { LayoutDefault } from "../layout/layout-default";
 import { ListCompatibleModel } from "../pages/list-compatible-model";
 import { SignIn } from "../pages/sign-in";
+import { TermsAndConditions } from "../pages/terms-and-conditions";
+import { PrivacyPolicy } from "../pages/privacy-policy";
+import { About } from "../pages/about";
+import { Contact } from "../pages/contact";
 
 export function AppRoutes() {
   return (
@@ -20,6 +24,11 @@ export function AppRoutes() {
         />
 
         <Route path="sign-in" element={<SignIn/>}/>
+
+        <Route path="/sobre" element={<About />}/>
+        <Route path="/contato" element={<Contact />}/>
+        <Route path="/termos-e-condicoes" element={<TermsAndConditions/>}/>
+        <Route path="/politica-de-privacidade" element={<PrivacyPolicy/>}/>
       </Route>
     </Routes>
   );
