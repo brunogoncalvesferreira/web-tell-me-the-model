f177c1a481f1fe55b64bad772ec48f85ecdab75a
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-10-23 18:01:18 -0300

    alterando layout tabela

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -24,12 +24,11 @@ export function Table({
         <thead>
           <tr className="text-left border-b border-zinc-700/30">
             <th className="p-4">Modelo</th>
-            <th className="p-4">Marca</th>
 
-            <th style={{ width: 100 }} className="p-4">
+            <th className="p-4">
               Compativeis
             </th>
-
+            <th style={{ width: 200 }} className="p-4">Marca</th>
             {user && (
               <th style={{ width: 200 }} className="p-4">
                 Criação compativeis
@@ -51,13 +50,13 @@ export function Table({
               className="border-b border-zinc-700/30 hover:bg-zinc-800/50 transition-colors"
             >
               <td className="p-4 font-bold uppercase">{film.model}</td>
-              <td className="p-4 uppercase">{film.mark}</td>
               <td className="p-4 text-left">
                 <button onClick={() => onGetAllCompatibleModel(film.id)}>
                   <SquareArrowOutUpRight className="size-5" />
                 </button>
               </td>
-
+              <td className="p-4 uppercase">{film.mark}</td>
+        
               {user && (
                 <td className="p-4 text-center">
                   <button onClick={() => onCreateCompatibleModel(film.id)}>
