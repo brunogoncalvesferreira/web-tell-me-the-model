e66173a5022b9617a8e158c6b7e7539852488870
Bruno <74840490+brunogoncalvesferreira@users.noreply.github.com>
2025-01-08 12:31:01 -0300

    Atualizar o axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://apitm.apis-vortice.online/",
+  baseURL: "https://apitm.apis-vortice.online",
   withCredentials: true,
 })
