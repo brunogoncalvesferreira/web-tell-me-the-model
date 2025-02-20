b7e4bdeb58d48237e1b83b3105c3f969fda61a50
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-02-20 16:53:10 -0300

    teste backend VPS

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  //baseURL: 'https://apitm.apis-vortice.online',
-  //baseURL: 'http://localhost:3000',
-  baseURL: 'https://api-tm3d.onrender.com',
+  baseURL: 'https://apitm.apis-vortice.online',
+  // baseURL: 'http://localhost:8080',
+  //baseURL: 'https://api-tm3d.onrender.com',
   withCredentials: true,
 })
 
-//export const fetchURL = 'https://apitm.apis-vortice.online'
-//export const fetchURL = 'http://localhost:3000'
-export const fetchURL = 'https://api-tm3d.onrender.com'
+export const fetchURL = 'https://apitm.apis-vortice.online'
+// export const fetchURL = 'http://localhost:8080'
+//export const fetchURL = 'https://api-tm3d.onrender.com'
